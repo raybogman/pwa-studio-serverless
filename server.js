d9d24d8f617922d5b8b5dea99f752457efb06a22
raybogman <bogman@bogman.info>
2021-04-25 11:59:41 +0200

    update 2

---
+++ server.js
@@ -0,0 +1,16 @@
+const { createUpwardServer } = require('@magento/upward-js');
+
+async function serve() {
+  await createUpwardServer({
+    port: 80,
+    bindLocal: true,
+    logUrl: true,
+    upwardPath: './upward.yml',
+    env: {
+      MAGENTO_BACKEND_URL: 'https://master-7rqtwti-mfwmkrjfqvbjk.us-4.magentosite.cloud/',
+      NODE_ENV: 'production'
+    }
+  });
+}
+
+serve();
